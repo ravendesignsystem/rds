c053b455bdff7598c80b0e03f990665848053b20
dannybrown73 <dannybee@gmail.com>
2019-01-21 23:27:28 -0500

    Readjusting folder structure.

--- src/_core/js/core.js
+++ src/_core/js/core.js
@@ -2,8 +2,8 @@
 import '../layouts/overlays/modal/modal';
 
 // core block scripts
-import '../blocks/masthead/masthead';
-import '../blocks/menu/menu';
+import '../../_blocks/masthead/masthead';
+import '../../_blocks/menu/menu';
 
 // core component scripts
 import '../components/content/accordion/accordion';
