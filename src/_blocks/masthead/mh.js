7b562e496ddfd4000285bed45864ecd557ef248a
Michael Ieradi <michael.ieradi@gmail.com>
2019-10-18 13:42:07 -0400

    change height on buttons

---
+++ src/_blocks/masthead/mh.js
@@ -0,0 +1,7 @@
+//* show masthead on scroll up
+
+const masthead = document.querySelector('#b-masthead');
+const header = document.querySelector('header');
+const body = document.body;
+
+export const Masthead = () => {};
--- src/_core/js/core.js
+++ src/_core/js/core.js
@@ -7,16 +7,28 @@ import '../../_components/menupopup/menupopup';
 
 // block scripts
 import '../../_blocks/alertheader/alertheader';
-import '../../_blocks/masthead/masthead';
+// import '../../_blocks/masthead/masthead';
+import { Masthead } from '../../_blocks/masthead/mh';
 import '../../_blocks/menu/menu';
+Masthead();
 
 // we should probably get rid of this before v1
 // but for old time sake...
-let pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+let pattern = [
+	'ArrowUp',
+	'ArrowUp',
+	'ArrowDown',
+	'ArrowDown',
+	'ArrowLeft',
+	'ArrowRight',
+	'ArrowLeft',
+	'ArrowRight',
+	'b',
+	'a',
+];
 let current = 0;
 
-let keyHandler = function (event) {
-
+let keyHandler = function(event) {
 	// If the key isn't in the pattern, or isn't the current key in the pattern, reset
 	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
 		current = 0;
@@ -30,8 +42,6 @@ let keyHandler = function (event) {
 		current = 0;
 		window.location.href = 'https://www.youtube.com/watch?v=RRwqftGrxf4';
 	}
-
 };
 
 document.addEventListener('keydown', keyHandler, false);
-
