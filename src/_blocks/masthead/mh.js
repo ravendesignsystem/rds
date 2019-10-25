3b58d45f80fe07492e9b51d170a4f98a18851bbe
Michael Ieradi <michael.ieradi@gmail.com>
2019-10-25 15:42:45 -0400

    conditionally hide secondary nav

--- src/_blocks/masthead/mh.js
+++ src/_blocks/masthead/mh.js
@@ -118,7 +118,7 @@ const showModal = type => {
 	Array.from(mastheadActionsCTA).map(el =>
 		el.classList.add('u-visually-hidden')
 	);
-	// hide masthead nav
+	// hide masthead nav if it does not include second level
 	if (!mastheadNav.classList.contains('masthead__second-level')) {
 		mastheadNav.classList.add('u-visually-hidden');
 	}
