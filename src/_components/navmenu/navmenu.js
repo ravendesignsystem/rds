f5ba380465c440814bb7cf63de7c667505a5763a
Troy Chaplin <troy.chaplin@gmail.com>
2019-11-05 09:35:49 -0500

    Added js for expand / collapse

--- src/_components/navmenu/navmenu.js
+++ src/_components/navmenu/navmenu.js
@@ -1,18 +1,66 @@
-const buttons = document.querySelectorAll('.has-submenu button');
-const buttonsArray = Array.from(buttons);
+let isMenuExpanded;
+let expanded;
+const navExpandButton = document.querySelector('.nav__expand button');
+const subMenuButtons = document.querySelectorAll(
+	'.c-nav--sidenav .has-submenu button'
+); // nodelist
+const subMenuButtonsArray = Array.from(subMenuButtons);
+const navMenuButtons = document.querySelectorAll(
+	'.c-nav--menubar .has-submenu button'
+); // nodelist
+const navMenuButtonsArray = Array.from(navMenuButtons);
 
-buttonsArray.map(button => {
-	button.addEventListener('click', e => {
-		const isExpanded = e.target.getAttribute('aria-expanded');
-		if (isExpanded === 'false') {
-			e.target.setAttribute('aria-expanded', true);
-			e.target.parentNode.classList.add('open');
+// function to toggle nav and side nav state
+const toggleMenuOpenState = buttonArray => {
+	buttonArray.map(button => {
+		button.addEventListener('click', e => {
+			const isExpanded = e.target.getAttribute('aria-expanded');
+			if (isExpanded === 'false') {
+				e.target.setAttribute('aria-expanded', true);
+				e.target.parentNode.classList.add('open');
+			} else {
+				e.target.setAttribute('aria-expanded', false);
+				e.target.parentNode.classList.remove('open');
+			}
+		});
+	});
+};
+
+toggleMenuOpenState(subMenuButtonsArray);
+toggleMenuOpenState(navMenuButtonsArray);
+
+// add click event listener to .nav__expand button target
+navExpandButton.addEventListener('click', e => {
+	// deconstruct the event for cleanliness
+	const { target } = e;
+	// assign isMenuExpanded to the tarets data attr
+	isMenuExpanded = target.getAttribute('data-expanded');
+
+	// if menu is expanded, change text and data attribute
+	if (isMenuExpanded === 'false') {
+		target.textContent = 'Collapse All';
+		target.setAttribute('data-expanded', true);
+	} else {
+		target.textContent = 'Expand All';
+		target.setAttribute('data-expanded', false);
+	}
+
+	// loop over menu buttons that have a parent of .has-menu
+	subMenuButtonsArray.map(subMenuButton => {
+		// assign expanded to the buttons data attribute
+		expanded = subMenuButton.getAttribute('aria-expanded');
+		// if menu is expanded, toggle aria
+		if (expanded === 'false') {
+			subMenuButton.setAttribute('aria-expanded', true);
 		} else {
-			e.target.setAttribute('aria-expanded', false);
-			e.target.parentNode.classList.remove('open');
+			subMenuButton.setAttribute('aria-expanded', false);
 		}
+		// if menu is expanded toggle class
+		subMenuButton.parentNode.classList.toggle('open');
 	});
 });
+
+// expandNav.addEventListener('click', expandMenu);
 
 // TODO
 // Can we add JS here shared by both Masthead and vertical nav
