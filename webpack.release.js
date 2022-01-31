e1dadc09dd27826bbbe4e84e3380c977573db3cd
marceloglacial <glacial@gmail.com>
2022-01-31 14:33:49 -0500

    update release

--- webpack.release.js
+++ webpack.release.js
@@ -7,7 +7,7 @@ const baseConfig = require('./webpack.config.js');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 const S3Plugin = require('webpack-s3-plugin');
 
-const version = '0.22.6';
+const version = '0.22.7';
 
 // Build Config
 module.exports = merge(baseConfig, {
